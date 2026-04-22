154df2b9906ecf55ef31576244ef6f2ba45dd0bc
Mark Hood <mark.a.hood@gmail.com>
2026-04-22 09:28:06 +0100

    app: Add writer activity placeholder

---
+++ src/activities/writer/WriterActivity.cpp
@@ -0,0 +1,34 @@
+#include "WriterActivity.h"
+
+#include <GfxRenderer.h>
+#include <I18n.h>
+
+#include "components/UITheme.h"
+#include "fontIds.h"
+
+void WriterActivity::onEnter() {
+  Activity::onEnter();
+  requestUpdate();
+}
+
+void WriterActivity::loop() {
+  if (mappedInput.isPressed(MappedInputManager::Button::Back)) {
+    finish();
+  }
+}
+
+void WriterActivity::render(RenderLock&&) {
+  renderer.clearScreen();
+
+  const auto& metrics = UITheme::getInstance().getMetrics();
+  const auto pageWidth = renderer.getScreenWidth();
+  const auto x = metrics.contentSidePadding;
+
+  GUI.drawHeader(renderer, Rect{0, metrics.topPadding, pageWidth, metrics.headerHeight}, tr(STR_WRITER));
+  renderer.drawText(UI_10_FONT_ID, x, metrics.topPadding + metrics.headerHeight + metrics.verticalSpacing, "Writer Mode PlaceHolder");
+
+  const auto labels = mappedInput.mapLabels(tr(STR_BACK), "", "", "");
+  GUI.drawButtonHints(renderer, labels.btn1, labels.btn2, labels.btn3, labels.btn4);
+
+  renderer.displayBuffer();
+}
