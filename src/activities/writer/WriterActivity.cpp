907e16fa0e83dbf46978de12dd41e9ce349e9d25
Mark Hood <mark.a.hood@gmail.com>
2026-04-22 14:41:07 +0100

    writer: add basic footer

--- src/activities/writer/WriterActivity.cpp
+++ src/activities/writer/WriterActivity.cpp
@@ -69,17 +69,69 @@ void WriterActivity::render(RenderLock&&) {
   }
 
   // Draw the screen
-  GUI.drawHeader(renderer, Rect{0, metrics.topPadding, pageWidth, metrics.headerHeight}, tr(STR_WRITER));
-
-  int y = metrics.topPadding + metrics.headerHeight + metrics.verticalSpacing;
+  int y = metrics.topPadding + metrics.verticalSpacing;
 
   for (const auto& line : visibleLines) {
     renderer.drawText(UI_10_FONT_ID, x, y, line.c_str());
     y += lineHeight;
   }
 
-  const auto labels = mappedInput.mapLabels(tr(STR_BACK), "Append", "", "");
-  GUI.drawButtonHints(renderer, labels.btn1, labels.btn2, labels.btn3, labels.btn4);
+  WriterActivity::renderFooter();
 
   renderer.displayBuffer();
+}
+
+int WriterActivity::countWords() const {
+  int words = 0;
+  bool inWord = false;
+
+  for (const unsigned char ch : draftText) {
+    if (std::isspace(ch)) {
+      inWord = false;
+    } else if (!inWord) {
+      words++;
+      inWord = true;
+    }
+  }
+
+  return words;
+}
+
+// Standard footer isn't writer-ready, so we'll keep our version local.
+void WriterActivity::renderFooter() const {
+  const auto& metrics = UITheme::getInstance().getMetrics();
+
+  int orientedMarginTop, orientedMarginRight, orientedMarginBottom, orientedMarginLeft;
+  renderer.getOrientedViewableTRBL(&orientedMarginTop, &orientedMarginRight, &orientedMarginBottom,
+                                   &orientedMarginLeft);
+
+  const auto screenHeight = renderer.getScreenHeight();
+  const auto footerY = screenHeight - metrics.statusBarVerticalMargin - orientedMarginBottom - 4;
+
+  // Battery display as per user settings
+  const bool showBatteryPercentage =
+      SETTINGS.hideBatteryPercentage == CrossPointSettings::HIDE_BATTERY_PERCENTAGE::HIDE_NEVER;
+
+  if (SETTINGS.statusBarBattery) {
+    GUI.drawBatteryLeft(renderer,
+                        Rect{metrics.statusBarHorizontalMargin + orientedMarginLeft + 1, footerY, metrics.batteryWidth,
+                             metrics.batteryHeight},
+                        showBatteryPercentage);
+  }
+
+  // Filename we're working on
+  std::string title = draftStore.getDraftDisplayName();
+  int titleWidth = renderer.getTextWidth(SMALL_FONT_ID, title.c_str());
+  renderer.drawText(SMALL_FONT_ID, (renderer.getScreenWidth() - titleWidth) / 2, footerY, title.c_str());
+
+  // Current wordcount
+  std::string wordCount = std::to_string(countWords()) + " words";
+  int wordCountWidth = renderer.getTextWidth(SMALL_FONT_ID, wordCount.c_str());
+
+  renderer.drawText(
+      SMALL_FONT_ID,
+      renderer.getScreenWidth() - metrics.statusBarHorizontalMargin - orientedMarginRight - wordCountWidth, footerY,
+      wordCount.c_str());
+
+  return;
 }
